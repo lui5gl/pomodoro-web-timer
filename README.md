35c0dcdb9f07ba833436600841a1bbf448feb9b4
lui5gl <lui5gl@outlook.com>
2024-08-10 10:36:31 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Pomodoro Timer
+
+A simple pomodoro timer that helps you stay focused and productive.
+
+## Overview
+
+The Pomodoro Timer is a productivity tool designed to help you manage your time efficiently. By breaking your work into intervals, traditionally 25 minutes in length, separated by short breaks, you can maintain high levels of focus and avoid burnout. After completing four intervals, you take a longer break to recharge.
+
+## Features
+
+- **Pomodoro Intervals**: Work for 25 minutes with a countdown timer.
+- **Short Breaks**: Take a 5-minute break after each Pomodoro interval.
+- **Long Breaks**: After four Pomodoro intervals, take a longer break of 15 minutes.
+- **Visual and Audio Alerts**: Receive notifications when it's time to work and when it's time to rest.
+- **Customizable Settings**: Adjust the lengths of work and break periods to fit your personal preferences.
+
+## Screenshots
+
+### Pomodoro
+
+![Image of pomodoro](/public/img/pomodoro.png)
+
+### Short Break
+
+![Image of short break](/public/img/pomodoro_short_break.png)
+
+### Long Break
+
+![Image of long break](/public/img/pomodoro_long_break.png)
+
+## Getting Started
+
+To get a local copy up and running, follow these simple steps.
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are greatly appreciated.
+
+Fork the Project
+Create your Feature Branch (git checkout -b feature/AmazingFeature)
+Commit your Changes (git commit -m 'Add some AmazingFeature')
+Push to the Branch (git push origin feature/AmazingFeature)
+Open a Pull Request
+
+## Contact
+
+Luis Gonzalez - <luisfernandogl55@gmail.com>
---
+++ app/components/pomodoro.tsx
@@ -0,0 +1,146 @@
+"use client";
+
+import Image from "next/image";
+import "pomodoro.module.css";
+import React, { useState, useEffect, useRef } from "react";
+
+export default function Pomodoro() {
+  const color_theme = useRef<HTMLDivElement>(null);
+  const reset_button = useRef<HTMLButtonElement>(null);
+  const notification_sound = useRef<HTMLAudioElement>(null);
+
+  const [current_minute, setCurrentMinute] = useState(25);
+  const [current_second, setCurrentSecond] = useState(0);
+
+  const [is_running, setIsRunning] = useState(false);
+  const [state_timer, setStateTimer] = useState("pomodoro");
+
+  // Set the title of the page to the current time
+  useEffect(() => {
+    document.title = `${current_minute}:${current_second < 10 ? `0${current_second}` : current_second} - Pomodoro Web Timer`;
+  }, [current_minute, current_second]);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    if (is_running) {
+      intervalId = setInterval(() => {
+        if (current_second === 0) {
+          if (current_minute === 0) {
+            clearInterval(intervalId);
+            if (notification_sound.current) {
+              notification_sound.current;
+            }
+          } else {
+            setCurrentMinute(current_minute - 1);
+            setCurrentSecond(59);
+          }
+        } else {
+          setCurrentSecond(current_second - 1);
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [is_running, current_minute, current_second]);
+
+  function handleIsRunningToggle() {
+    setIsRunning(!is_running);
+    reset_button.current?.classList.remove("hidden");
+  }
+
+  function reset() {
+    setIsRunning(false);
+    changeStatePomodoro(state_timer);
+    reset_button.current?.classList.add("hidden");
+  }
+
+  function changeStatePomodoro(state: string) {
+    setStateTimer(state);
+    setIsRunning(false);
+
+    color_theme.current?.classList.remove(
+      "pomodoro",
+      "short-break",
+      "long-break",
+    );
+
+    color_theme.current?.classList.add(state);
+    reset_button.current?.classList.add("hidden");
+
+    switch (state) {
+      case "pomodoro":
+        setCurrentMinute(25);
+        setCurrentSecond(0);
+        break;
+
+      case "short-break":
+        setCurrentMinute(5);
+        setCurrentSecond(0);
+        break;
+
+      case "long-break":
+        setCurrentMinute(15);
+        setCurrentSecond(0);
+        break;
+    }
+  }
+
+  return (
+    <section
+      ref={color_theme}
+      className="pomodoro flex min-h-svh select-none flex-col items-center justify-center gap-2 bg-gradient-to-b p-5 text-white"
+    >
+      <h1 className="text-center text-5xl font-bold">Pomodoro</h1>
+      <div className="w-full max-w-xl rounded-sm bg-white/25 p-8">
+        <div className="relative flex w-full items-center justify-center">
+          <h2 className="drop-shadow-title text-center text-9xl font-bold">
+            {`${current_minute}:${current_second < 10 ? `0${current_second}` : current_second}`}
+          </h2>
+          <button
+            ref={reset_button}
+            onClick={reset}
+            className="absolute right-0 hidden place-self-center transition-all duration-150"
+          >
+            <Image src={"/icon/arrow.svg"} alt="reset" width={18} height={18} />
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2">
+          <button
+            id="playButton"
+            onClick={handleIsRunningToggle}
+            className="col-span-3"
+          >
+            {is_running ? "Stop" : "Start"}
+          </button>
+
+          <button
+            id="button-pomodoro"
+            onClick={() => changeStatePomodoro("pomodoro")}
+          >
+            Pomodoro
+          </button>
+          <button
+            id="button-short-break"
+            onClick={() => changeStatePomodoro("short-break")}
+          >
+            Short Break
+          </button>
+          <button
+            id="button-long-break"
+            onClick={() => changeStatePomodoro("long-break")}
+          >
+            Long Break
+          </button>
+        </div>
+      </div>
+      {/* Notification end task */}
+      <audio
+        ref={notification_sound}
+        id="audio"
+        src="sound/alarm.wav"
+        preload="auto"
+      />
+    </section>
+  );
+}
