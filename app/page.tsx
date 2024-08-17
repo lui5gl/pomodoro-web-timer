948df8f538e27a3ebf1dc820641a19370600a5ef
lui5gl <lui5gl@outlook.com>
2024-08-17 13:07:58 -0600

    feat: sort imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import { useRef } from "react";
 
 import Timer from "@app/components/timer";
