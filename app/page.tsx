43da1a8258985cde3c5e2166b75f346f9a1a1281
lui5gl <lui5gl@outlook.com>
2025-01-04 09:58:59 -0600

    refactor: update layout structure in Home component for improved responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -94,8 +94,8 @@ export default function Home() {
       className="flex min-h-svh select-none flex-col items-center justify-center gap-2 bg-gradient-to-b p-5 text-white"
     >
       <h1 className="text-center text-5xl font-bold">Pomodoro</h1>
-      <section className="w-full max-w-xl rounded-sm bg-white/25 p-8">
-        <div className="relative flex w-full items-center justify-center">
+      <section className="grid w-full max-w-xl grid-cols-3 gap-2 rounded-sm bg-white/25 p-8">
+        <div className="relative col-span-3 flex w-full items-center justify-center">
           <h2 className="text-center text-9xl font-bold drop-shadow-timer">
             {`${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}
           </h2>
@@ -107,32 +107,30 @@ export default function Home() {
             <Image src={"/icon/arrow.svg"} alt="reset" width={18} height={18} />
           </button>
         </div>
-        <div className="grid grid-cols-3 gap-2">
-          <button onClick={handleToggleIsRunning} className="col-span-3">
-            {isRunning ? "Stop" : "Start"}
-          </button>
 
-          <button
-            id="button-pomodoro"
-            onClick={() => handleChangeState("pomodoro")}
-          >
-            Pomodoro
-          </button>
-          <button
-            id="button-short-break"
-            onClick={() => handleChangeState("short-break")}
-          >
-            Short Break
-          </button>
-          <button
-            id="button-long-break"
-            onClick={() => handleChangeState("long-break")}
-          >
-            Long Break
-          </button>
-        </div>
+        <button onClick={handleToggleIsRunning} className="col-span-3">
+          {isRunning ? "Stop" : "Start"}
+        </button>
+
+        <button
+          id="button-pomodoro"
+          onClick={() => handleChangeState("pomodoro")}
+        >
+          Pomodoro
+        </button>
+        <button
+          id="button-short-break"
+          onClick={() => handleChangeState("short-break")}
+        >
+          Short Break
+        </button>
+        <button
+          id="button-long-break"
+          onClick={() => handleChangeState("long-break")}
+        >
+          Long Break
+        </button>
       </section>
-
       <audio ref={notificationSoundRef} src="/sound/alarm.wav" preload="auto" />
     </main>
   );
