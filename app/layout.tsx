67cd7eb32bb0e443ffc4448f9ddb2b8d1a63bac9
lui5gl <lui5gl@outlook.com>
2025-01-13 15:05:21 -0600

    fix: add antialiased class to body for improved text rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={onest.className}>{children}</body>
+      <body className={`${onest.className} antialiased`}>{children}</body>
     </html>
   );
 }
