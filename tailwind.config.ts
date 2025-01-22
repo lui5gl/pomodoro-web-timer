7252e1f2eada3d4ad918af0c18bc9438ab1e5ba9
lui5gl <lui5gl@outlook.com>
2025-01-22 09:49:48 -0600

    fix: adjust drop shadow opacity for hour in tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
         box: "4px 4px 0 rgb(255 255 255 / 0.75)",
       },
       dropShadow: {
-        hour: "6px 6px 0 rgb(255 255 255 / 0.25)",
+        hour: "6px 6px 0 rgb(255 255 255 / 0.4)",
       },
     },
   },
